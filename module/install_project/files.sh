2f3ece8e7d81a453a5f31e593c58c814ee3608a0
roozbeh <roozbehp.shafiee@gmail.com>
2020-05-01 11:41:06 +0430

    redux files are added to project

--- module/install_project/files.sh
+++ module/install_project/files.sh
@@ -32,3 +32,31 @@ fi
 # create fetch.test.ts
 cat "$APIS_FILE_TEST">"$MAIN_DIRECTORY/$PROJECT_NAME/src/apis/__tests__/fetch.test.ts"
 failFunction $? "fetch.test.ts created" "fetch.test.ts can not creat"
+
+
+
+
+# redux store and combine files
+STORE="$MAIN_DIRECTORY/module/install_project/dependencies/redux/store.ts"
+STORE_TEST="$MAIN_DIRECTORY/module/install_project/dependencies/redux/store.test.ts"
+COMBINE="$MAIN_DIRECTORY/module/install_project/dependencies/redux/combine.ts"
+COMBINE_TEST="$MAIN_DIRECTORY/module/install_project/dependencies/redux/combine.test.ts"
+
+# create store
+cat "$STORE">"$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/store.ts"
+failFunction $? "store.ts created" "store.ts can not creat"
+# create combine
+cat "$COMBINE">"$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/combine.ts"
+failFunction $? "combine.ts created" "combine.ts can not creat"
+# create __tests__ dir
+if [ ! -d "$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/__tests__" ]
+then
+    mkdir "$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/__tests__"
+    failFunction $? "__tests__ dir created" "__tests_ dir can not creat"
+fi
+# create store.test.ts
+cat "$STORE_TEST">"$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/__tests__/store.test.ts"
+failFunction $? "store.test.ts created" "store.test.ts can not creat"
+# create combine.test.ts
+cat "$COMBINE_TEST">"$MAIN_DIRECTORY/$PROJECT_NAME/src/redux/__tests__/combine.test.ts"
+failFunction $? "combine.test.ts created" "combine.test.ts can not creat"
--- tests/install_project/files.sh
+++ tests/install_project/files.sh
@@ -18,4 +18,35 @@ then
     exit 1
 fi
 
+# test redux files 
+STORE="$ROOT_DIRECTORY/module/install_project/dependencies/redux/store.ts"
+STORE_TEST="$ROOT_DIRECTORY/module/install_project/dependencies/redux/store.test.ts"
+COMBINE="$ROOT_DIRECTORY/module/install_project/dependencies/redux/combine.ts"
+COMBINE_TEST="$ROOT_DIRECTORY/module/install_project/dependencies/redux/combine.test.ts"
+
+if [ ! -f "$STORE" ]
+then
+    echo "store.ts dose NOT exists"
+    exit 1
+fi
+
+if [ ! -f "$STORE_TEST" ]
+then 
+    echo "store.test.ts dose NOT exists"
+    exit 1
+fi
+
+if [ ! -f "$COMBINE" ]
+then
+    echo "combine.ts dose NOT exists"
+    exit 1
+fi
+
+if [ ! -f "$COMBINE_TEST" ]
+then 
+    echo "combine.test.ts dose NOT exists"
+    exit 1
+fi
+
+
 exit 0
